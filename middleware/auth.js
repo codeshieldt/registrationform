3414c553dc78b410bccf7d36f2a46773652ddf7b
Dilawar Khan Niazi <dilawar.niazi.dn@gmail.com>
2022-08-04 10:01:06 +0500

    add middleware auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = function auth(req, res, next)
+{
+    const token = req.header('token');
+    if(!token) return res.status(401)
+        .redirect('./login.html');
+    
+    try{
+        const decoded = jwt = jwt.verify(token, process.env.jwtPrivateKey);
+        req.user = decoded;
+        next();
+    }
+    catch(ex)
+    {
+        res.status(400).redirect('/login.html')
+    }
+}
